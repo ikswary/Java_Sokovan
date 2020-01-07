c813c3e8603c5451efa1085428e5c9b90e504cee
ikswary <ikswary@naver.com>
2020-01-07 16:20:40 +0900

    [FIX] 맵 로딩시 깊은 복사를 하도록 변경

--- src/domain/Sokovan.java
+++ src/domain/Sokovan.java
@@ -1,16 +1,21 @@
 package domain;
 
+import java.util.Arrays;
+
 public class Sokovan {
 	private Player player;
 	private int stage;
 	private char[][] map;
 
 	public Sokovan() {
-		player = new Player();
+		this.player = new Player();
+		this.map = new char[8][8];
 	}
 
 	public void initStage(int stage) {
-		this.map = Stage.map[stage - 1];
+		for (int i = 0; i < this.map.length; i++) {
+			this.map[i] = Stage.map[stage - 1][i].clone();
+		}
 		this.player.setAxis(Stage.axis[stage - 1]);
 	}
 
