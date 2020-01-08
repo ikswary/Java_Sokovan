b86be99c776814197663a93d9322a525ed891035
ikswary <ikswary@naver.com>
2020-01-08 14:21:31 +0900

    [ADD] Stage와 Sokovan 클래스에 클리어 좌표 생성

--- src/domain/Sokovan.java
+++ src/domain/Sokovan.java
@@ -1,22 +1,22 @@
 package domain;
 
-import java.util.Arrays;
-
 public class Sokovan {
 	private Player player;
 	private int stage;
 	private char[][] map;
+	private int [][] clearAxis;
 
 	public Sokovan() {
 		this.player = new Player();
 		this.map = new char[8][8];
+		this.clearAxis = new int[3][2];
 	}
 
 	public void initStage(int stage) {
 		for (int i = 0; i < this.map.length; i++) {
 			this.map[i] = Stage.map[stage - 1][i].clone();
 		}
-		this.player.setAxis(Stage.axis[stage - 1]);
+		this.player.setAxis(Stage.playerAxis[stage - 1]);
 	}
 
 	public void printMap() {
--- src/domain/Stage.java
+++ src/domain/Stage.java
@@ -2,7 +2,7 @@ package domain;
 
 public class Stage {
 	final static char[][][] map = {
-			{
+			{   //stage1의 맵
 					{'#', '#', '#', '#', '#', '#', '#', '#'},
 					{'#', '#', 'P', ' ', '#', '#', '#', '#'},
 					{'#', '#', ' ', 'B', ' ', ' ', '#', '#'},
@@ -33,7 +33,18 @@ public class Stage {
 					{'#', '#', '#', '#', '#', '#', '#', '#'}
 			}
 	};
-	final static Integer[][] axis = {{1, 2}, {0, 0}, {0, 0}};
+	final static Integer[][] playerAxis = {{1, 2}, {0, 0}, {0, 0}};
+	final static Integer[][][] clearAxis = {
+			{
+					{4, 1}, {5, 1}, {6, 1} // Stage1의 클리어 좌표
+			},
+			{
+					{0, 0}, {1, 1}, {2, 3}
+			},
+			{
+					{0, 0}, {1, 1}, {2, 3}
+			}
+	};
 }
 
 
