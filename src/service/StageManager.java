9160c4e4b2bd00261000ffeee234782a1fab0373
ikswary <ikswary@naver.com>
2020-01-18 18:47:10 +0900

    FIX : 실제로 움직였을때만 움직인 횟수가 올라가도록 수정
    맵 로딩중 움직여지는 버그 수정 필요

--- src/service/StageManager.java
+++ src/service/StageManager.java
@@ -1,144 +1,147 @@
 package service;
 
 import domain.*;
+import view.ConsoleOutput;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class StageManager {
-	private Player player;
-	private Map map;
-	private List<SnapShot> snapShots;
-	private Integer currentStage;
+    private Player player;
+    private Map map;
+    private List<SnapShot> snapShots;
+    private Integer currentStage;
 
 
-	public StageManager() {
-		this.player = new Player();
-		this.map = new Map();
+    public StageManager() {
+        this.player = new Player();
+        this.map = new Map();
         this.snapShots = new ArrayList<>();
         this.currentStage = 0;
-	}
+    }
 
-	public void initStage(Integer mapNumber) {
-		this.currentStage++;
-		map.setMap(mapNumber);
-		this.player.setAxis(MapDB.playerAxis[mapNumber]);
-		printGameScreen();
-	}
+    public void initStage(Integer mapNumber) {
+        this.currentStage++;
+        map.setMap(mapNumber);
+        this.player.setAxis(MapDB.playerAxis[mapNumber]);
+		ConsoleOutput.printChangingStage();
+        printGameScreen();
+    }
 
-	private Boolean isAxisEmpty(Integer axisX, Integer axisY) {
-		return map.getMap()[axisX][axisY].equals(' ')
-				|| map.getMap()[axisX][axisY].equals('*');
-	}
+    private Boolean isAxisEmpty(Integer axisX, Integer axisY) {
+        return map.getMap()[axisX][axisY].equals(' ')
+                || map.getMap()[axisX][axisY].equals('*');
+    }
 
-	private Boolean isAxisBox(Integer axisX, Integer axisY) {
-		return map.getMap()[axisX][axisY].equals('B');
-	}
+    private Boolean isAxisBox(Integer axisX, Integer axisY) {
+        return map.getMap()[axisX][axisY].equals('B');
+    }
 
-	private void performMove(Integer currentAxisX, Integer currentAxisY
-			, Integer postAxisX, Integer postAxisY) {
-		map.moveByIndex(currentAxisX, currentAxisY, postAxisX, postAxisY);
-		player.setAxis(new Integer[]{postAxisX, postAxisY});
-	}
-
-	public Boolean movePlayer(Cursor cursor) {
-		Integer axisX = player.getAxisX();
-		Integer axisY = player.getAxisY();
-		final Integer AXIS_UP = axisX - 1;
-		final Integer AXIS_DOWN = axisX + 1;
-		final Integer AXIS_LEFT = axisY - 1;
-		final Integer AXIS_RIGHT = axisY + 1;
-		final Integer AXIS_UPX2 = axisX - 2;
-		final Integer AXIS_DOWNX2 = axisX + 2;
-		final Integer AXIS_LEFTX2 = axisY - 2;
-		final Integer AXIS_RIGHTX2 = axisY + 2;
-
-		if (cursor.equals(Cursor.UP)) {
-			if (isAxisEmpty(AXIS_UP, axisY)) {
-				takeSnapShot();
-				performMove(axisX, axisY, AXIS_UP, axisY);
-			} else if (isAxisBox(AXIS_UP, axisY)) {
-				if (isAxisEmpty(AXIS_UPX2, axisY)) {
-					takeSnapShot();
-					map.moveByIndex(AXIS_UP, axisY, AXIS_UPX2, axisY);
-					performMove(axisX, axisY, AXIS_UP, axisY);
-				}
-			}
-		} else if (cursor.equals(Cursor.DOWN)) {
-			if (isAxisEmpty(AXIS_DOWN, axisY)) {
-				takeSnapShot();
-				performMove(axisX, axisY, AXIS_DOWN, axisY);
-			} else if (isAxisBox(AXIS_DOWN, axisY)) {
-				if (isAxisEmpty(AXIS_DOWNX2, axisY)) {
-					takeSnapShot();
-					map.moveByIndex(AXIS_DOWN, axisY, AXIS_DOWNX2, axisY);
-					performMove(axisX, axisY, AXIS_DOWN, axisY);
-					// 아래로 밀고 이동
-				}
-			}
-		} else if (cursor.equals(Cursor.LEFT)) {
-			if (isAxisEmpty(axisX, AXIS_LEFT)) {
-				takeSnapShot();
-				performMove(axisX, axisY, axisX, AXIS_LEFT);
-			} else if (isAxisBox(axisX, AXIS_LEFT)) {
-				if (isAxisEmpty(axisX, AXIS_LEFTX2)) {
-					takeSnapShot();
-					map.moveByIndex(axisX, AXIS_LEFT, axisX, AXIS_LEFTX2);
-					performMove(axisX, axisY, axisX, AXIS_LEFT);
-					// 왼쪽으로 밀고 이동
-				}
-			}
-		} else if (cursor.equals(Cursor.RIGHT)) {
-			if (isAxisEmpty(axisX, AXIS_RIGHT)) {
-				takeSnapShot();
-				performMove(axisX, axisY, axisX, AXIS_RIGHT);
-			} else if (isAxisBox(axisX, AXIS_RIGHT)) {
-				if (isAxisEmpty(axisX, AXIS_RIGHTX2)) {
-					takeSnapShot();
-					map.moveByIndex(axisX, AXIS_RIGHT, axisX, AXIS_RIGHTX2);
-					performMove(axisX, axisY, axisX, AXIS_RIGHT);
-					// 위로 밀고 이동
-				}
-			}
-		} else if (cursor.equals(Cursor.BACK_SPACE)) {
-			this.player.movedBackward();
-			loadSnapShot();
-		}
-
-		if (map.isStageClear()) {
-			for (int i = 0; i < 3; i++) {
-				this.snapShots.remove(0);
-			}
-			return true;
-		}
+    private void performMove(Integer currentAxisX, Integer currentAxisY
+            , Integer postAxisX, Integer postAxisY) {
+        map.moveByIndex(currentAxisX, currentAxisY, postAxisX, postAxisY);
+        player.setAxis(new Integer[]{postAxisX, postAxisY});
 		this.player.movedOnce();
-		printGameScreen();
-		return false;
-	}
+    }
 
-	public void takeSnapShot() {
-		if (snapShots.size() > 2) {
-			snapShots.remove(0);
-		}
-		snapShots.add(new SnapShot(player.cloneAxis(), map.cloneMap()));
-	}
+    public Boolean movePlayer(Cursor cursor) {
+        Integer axisX = player.getAxisX();
+        Integer axisY = player.getAxisY();
+        final Integer AXIS_UP = axisX - 1;
+        final Integer AXIS_DOWN = axisX + 1;
+        final Integer AXIS_LEFT = axisY - 1;
+        final Integer AXIS_RIGHT = axisY + 1;
+        final Integer AXIS_UPX2 = axisX - 2;
+        final Integer AXIS_DOWNX2 = axisX + 2;
+        final Integer AXIS_LEFTX2 = axisY - 2;
+        final Integer AXIS_RIGHTX2 = axisY + 2;
 
-	public void loadSnapShot() {
-		try {
-			SnapShot snapShot = snapShots.remove(snapShots.size() - 1);
-			player.setAxis(snapShot.getPlayerAxis());
-			map.setMap(snapShot.getMap());
-		} catch (ArrayIndexOutOfBoundsException e) {
-		}
+        if (cursor.equals(Cursor.UP)) {
+            if (isAxisEmpty(AXIS_UP, axisY)) {
+                takeSnapShot();
+                performMove(axisX, axisY, AXIS_UP, axisY);
+            } else if (isAxisBox(AXIS_UP, axisY)) {
+                if (isAxisEmpty(AXIS_UPX2, axisY)) {
+                    takeSnapShot();
+                    map.moveByIndex(AXIS_UP, axisY, AXIS_UPX2, axisY);
+                    performMove(axisX, axisY, AXIS_UP, axisY);
+                }
+            }
+        } else if (cursor.equals(Cursor.DOWN)) {
+            if (isAxisEmpty(AXIS_DOWN, axisY)) {
+                takeSnapShot();
+                performMove(axisX, axisY, AXIS_DOWN, axisY);
+            } else if (isAxisBox(AXIS_DOWN, axisY)) {
+                if (isAxisEmpty(AXIS_DOWNX2, axisY)) {
+                    takeSnapShot();
+                    map.moveByIndex(AXIS_DOWN, axisY, AXIS_DOWNX2, axisY);
+                    performMove(axisX, axisY, AXIS_DOWN, axisY);
+                    // 아래로 밀고 이동
+                }
+            }
+        } else if (cursor.equals(Cursor.LEFT)) {
+            if (isAxisEmpty(axisX, AXIS_LEFT)) {
+                takeSnapShot();
+                performMove(axisX, axisY, axisX, AXIS_LEFT);
+            } else if (isAxisBox(axisX, AXIS_LEFT)) {
+                if (isAxisEmpty(axisX, AXIS_LEFTX2)) {
+                    takeSnapShot();
+                    map.moveByIndex(axisX, AXIS_LEFT, axisX, AXIS_LEFTX2);
+                    performMove(axisX, axisY, axisX, AXIS_LEFT);
+                    // 왼쪽으로 밀고 이동
+                }
+            }
+        } else if (cursor.equals(Cursor.RIGHT)) {
+            if (isAxisEmpty(axisX, AXIS_RIGHT)) {
+                takeSnapShot();
+                performMove(axisX, axisY, axisX, AXIS_RIGHT);
+            } else if (isAxisBox(axisX, AXIS_RIGHT)) {
+                if (isAxisEmpty(axisX, AXIS_RIGHTX2)) {
+                    takeSnapShot();
+                    map.moveByIndex(axisX, AXIS_RIGHT, axisX, AXIS_RIGHTX2);
+                    performMove(axisX, axisY, axisX, AXIS_RIGHT);
+                    // 위로 밀고 이동
+                }
+            }
+        } else if (cursor.equals(Cursor.BACK_SPACE)) {
+            this.player.movedBackward();
+            loadSnapShot();
+        }
 
-	}
+        if (map.isStageClear()) {
+            for (int i = 0; i < 3; i++) {
+                this.snapShots.remove(0);
+            }
+            return true;
+        }
+        printGameScreen();
+        return false;
+    }
 
-	public void printGameScreen() {
-		System.out.println("┌──────────┐");
-		System.out.println(" Stage : " + this.currentStage);
-		System.out.println("  Map  : " + this.map.getMapNumber());
-		System.out.println("  Move : " + this.player.getNumberOfMove());
-		System.out.println("└──────────┘");
-		map.printMap();
-	}
+    public void takeSnapShot() {
+        if (snapShots.size() > 2) {
+            snapShots.remove(0);
+        }
+        snapShots.add(new SnapShot(player.cloneAxis(), map.cloneMap()));
+    }
+
+    public void loadSnapShot() {
+        try {
+            SnapShot snapShot = snapShots.remove(snapShots.size() - 1);
+            player.setAxis(snapShot.getPlayerAxis());
+            map.setMap(snapShot.getMap());
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+
+    }
+
+    public void printGameScreen() {
+		System.out.println("\n\n\n\n\n");
+        System.out.println("┌──────────┐");
+        System.out.println(" Stage : " + this.currentStage);
+        System.out.println("  Map  : " + this.map.getMapNumber());
+        System.out.println("  Move : " + this.player.getNumberOfMove());
+        System.out.println("└──────────┘");
+        map.printMap();
+    }
 }
